4d8b1df86ec016d750eb5a286f9a72ab24ea71c2
Takai-Kenji <takai_kenji@retail-ai.jp>
2022-09-21 16:25:09 +0900

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     title: `gatsby`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+  "gatsby-plugin-image",
+  "gatsby-plugin-sharp",
+  ],
+};
